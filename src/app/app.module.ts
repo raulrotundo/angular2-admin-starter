2b0d311f2cf38b0a9f86586b41da0cf6d85e4745
Raul Rotundo Globant <raulrotundo@gmail.com>
2017-02-07 14:52:09 -0300

    New admin sb layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent }  from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './heroe-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './services/hero.service';
+import { ExpenseReportComponent } from './expense-report/expense-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    ExpenseReportComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
